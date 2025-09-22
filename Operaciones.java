34f56a2d3313644b06838d71f10c39963c496c57
SuMartukiRechulona5555 <marta.duran.gemes.alu@iesjulioverne.es>
2025-09-22 13:47:13 +0200

    Division

---
+++ Operaciones.java
@@ -0,0 +1,9 @@
+public double dividir(double a, double b) {
+    if (b == 0) { 
+        System.out.println("No se puede dividir por cero.");
+        return 0; 
+    } else {
+        return a / b;
+    }
+}
+
