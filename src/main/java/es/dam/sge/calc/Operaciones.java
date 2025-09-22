db80a7449d4b3b7f4cb3c54c9f805299325f60cd
SuMartukiRechulona5555 <marta.duran.gemes.alu@iesjulioverne.es>
2025-09-22 14:04:22 +0200

    Division añadida

--- src/main/java/es/dam/sge/calc/Operaciones.java
+++ src/main/java/es/dam/sge/calc/Operaciones.java
@@ -6,3 +6,12 @@ public class Operaciones {
     public static double sumar(double a, double b) {
         return a + b;
     }
+
+public double dividir(double a, double b) {
+    if (b == 0) { 
+        System.out.println("No se puede dividir por cero.");
+        return 0; 
+    } else {
+        return a / b;
+    }
+}
