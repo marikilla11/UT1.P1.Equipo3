d5edc272ce6e5020452d32cdcdaa58ac5d7fe8c6
JuanPerFer <juanernesto.perez.fernandez.alu@iesjulioverne.es>
2025-09-23 08:29:42 +0200

    Raíz cuadrada añadida

--- src/main/java/es/dam/sge/calc/Operaciones.java
+++ src/main/java/es/dam/sge/calc/Operaciones.java
@@ -6,3 +6,11 @@ public class Operaciones {
     public static double sumar(double a, double b) {
         return a + b;
     }
+
+    public double raizCuadrada(double numero) { 
+        if (numero < 0) {
+            throw new IllegalArgumentException("No se puede calcular raíz de número negativo");
+        } 
+        return Math.sqrt(numero);
+    }
+}
