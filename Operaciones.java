b1a8bf39f1dd0f663d5caa8dc0adc52e355b910a
JuanPerFer <juanernesto.perez.fernandez.alu@iesjulioverne.es>
2025-09-23 09:08:07 +0200

    Archivo adicional eliminado

--- Operaciones.java
+++
@@ -1,9 +0,0 @@
-public double dividir(double a, double b) {
-    if (b == 0) { 
-        System.out.println("No se puede dividir por cero.");
-        return 0; 
-    } else {
-        return a / b;
-    }
-}
-
